f6698d28568ebf032ea45452ea1049e7487b3d54
Sergio Bezerra da Silva <50722925+sergiobsilva2505@users.noreply.github.com>
2023-11-13 23:31:48 -0300

    first commit

---
+++ src/main/java/br/com/fiap/techFlix/TechFlixApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/techFlix/TechFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.techFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
