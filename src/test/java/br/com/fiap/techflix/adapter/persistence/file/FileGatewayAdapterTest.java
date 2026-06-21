81cbafd966ae2847ce1cafbcd7a16e0d0e19b8b6
Sergio Bezerra <sergio.bezerra@outlook.com>
2026-06-21 19:11:33 -0300

    test: improve error handling in FileGatewayAdapterTest using StepVerifier

--- src/test/java/br/com/fiap/techflix/adapter/persistence/file/FileGatewayAdapterTest.java
+++ src/test/java/br/com/fiap/techflix/adapter/persistence/file/FileGatewayAdapterTest.java
@@ -1,30 +1,34 @@
 package br.com.fiap.techflix.adapter.persistence.file;
 
 import br.com.fiap.techflix.domain.entities.file.File;
+import br.com.fiap.techflix.domain.entities.file.FileNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import br.com.fiap.techflix.adapter.persistence.file.exception.FileSizeExceededException;
+import org.apache.tomcat.util.http.fileupload.InvalidFileNameException;
+import java.io.IOException;
+
 import static org.mockito.Mockito.*;
 
-public class FileGatewayAdapterTest {
+class FileGatewayAdapterTest {
 
     private FileRepository fileRepository;
     private MultipartFile multipartFile;
     private FileGatewayAdapter fileGatewayAdapter;
 
     @BeforeEach
-    public void setup() {
+    void setup() {
         fileRepository = mock(FileRepository.class);
         multipartFile = mock(MultipartFile.class);
         fileGatewayAdapter = new FileGatewayAdapter(fileRepository);
     }
 
     @Test
-    public void findByIdReturnsFileContentWhenExists() {
+    void findByIdReturnsFileContentWhenExists() {
         String fileId = "123";
         FileDocument document = new FileDocument("123", "test.txt", "video/mp4", 3, new byte[]{1, 2, 3});
         when(fileRepository.findById(fileId)).thenReturn(Mono.just(document));
@@ -35,16 +39,17 @@ public class FileGatewayAdapterTest {
     }
 
     @Test
-    public void findByIdReturnsEmptyWhenDoesNotExist() {
+    void findByIdReturnsEmptyWhenDoesNotExist() {
         String fileId = "123";
         when(fileRepository.findById(fileId)).thenReturn(Mono.empty());
         Mono<byte[]> result = fileGatewayAdapter.findById(fileId);
         StepVerifier.create(result)
-                .verifyComplete();
+                .expectError(FileNotFoundException.class)
+                .verify();
     }
 
     @Test
-    public void saveAttachmentSuccessfully() throws Exception {
+    void saveAttachmentSuccessfully() throws Exception {
         String fileName = "test.txt";
         when(multipartFile.getOriginalFilename()).thenReturn(fileName);
         when(multipartFile.getBytes()).thenReturn(new byte[]{1, 2, 3});
@@ -57,18 +62,40 @@ public class FileGatewayAdapterTest {
     }
 
     @Test
-    public void saveAttachmentFailsWhenFileNameContainsInvalidPath() throws Exception {
+    void saveAttachmentFailsWhenFileNameContainsInvalidPath() {
         String fileName = "../test.txt";
         when(multipartFile.getOriginalFilename()).thenReturn(fileName);
-        assertThrows(Exception.class, () -> fileGatewayAdapter.saveAttachment(multipartFile));
+
+        Mono<File> result = Mono.defer(() -> {
+            try {
+                return fileGatewayAdapter.saveAttachment(multipartFile);
+            } catch (Exception e) {
+                return Mono.error(e);
+            }
+        });
+
+        StepVerifier.create(result)
+                .expectError(InvalidFileNameException.class)
+                .verify();
     }
 
     @Test
-    public void saveAttachmentFailsWhenFileSizeExceedsLimit() throws Exception {
+    void saveAttachmentFailsWhenFileSizeExceedsLimit() throws IOException {
         String fileName = "test.txt";
         byte[] largeFileContent = new byte[1024 * 1024 * 16]; // 16MB
         when(multipartFile.getOriginalFilename()).thenReturn(fileName);
         when(multipartFile.getBytes()).thenReturn(largeFileContent);
-        assertThrows(Exception.class, () -> fileGatewayAdapter.saveAttachment(multipartFile));
+
+        Mono<File> result = Mono.defer(() -> {
+            try {
+                return fileGatewayAdapter.saveAttachment(multipartFile);
+            } catch (Exception e) {
+                return Mono.error(e);
+            }
+        });
+
+        StepVerifier.create(result)
+                .expectError(FileSizeExceededException.class)
+                .verify();
     }
 }
